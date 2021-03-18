2e79ca0f3c54aaf5548f554f44d9be2520a1bbe3
lepouki <pouki@mailo.com>
2021-03-18 18:43:01 +0100

    Begin refactor

--- src/main.c
+++ src/main.c
@@ -28,12 +28,12 @@ typedef struct {
   int samples;
 } gm_ImageConfig;
 
-gm_Status gm_WriteToOutput(const gm_ImageConfig *config);
+gm_Status gm_Run(const gm_ImageConfig *config);
 void gm_PrintStatus(FILE *stream, int status_code);
 
 int main() {
   const gm_ImageConfig kConfig = {.size = {.x = 500, .y = 500}, .samples = 8};
-  const int kStatus = gm_WriteToOutput(&kConfig);
+  const int kStatus = gm_Run(&kConfig);
 
   if (kStatus) {
     gm_PrintStatus(stderr, kStatus);
@@ -44,127 +44,41 @@ int main() {
   return kStatus;
 }
 
-// clang-format off
-const char *const kVertexShaderSource =
-    "#version 330 core \n"
+#define GM_FORCE_TRAILING_SEMICOLON_ (void)0
 
-    "layout(location = 0) in vec2 a_Position;\n"
-    "layout(location = 1) in vec2 a_TexCoords;\n"
-
-    "out vec2 v_TexCoords;\n"
-
-    "void main() {\n"
-      "v_TexCoords = a_TexCoords;\n"
-      "gl_Position = vec4(a_Position, 1.0, 1.0);\n"
-    "}\n";
-
-const char *const kFragmentShaderSource =
-    "#version 330 core\n"
-
-    "in vec2 v_TexCoords;\n"
-
-    "out vec4 f_Color;\n"
-
-    "vec2 ComplexMul(vec2 a, vec2 b) {\n"
-      "return vec2(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x);\n"
-    "}\n"
-
-    "vec2 ComplexSq(vec2 z) {\n"
-      "return ComplexMul(z, z);\n"
-    "}\n"
-
-    "float ComplexSqMag(vec2 z) {\n"
-      "return z.x * z.x + z.y * z.y;\n"
-    "}\n"
-
-    "vec3 HsvToRgb(vec3 hsv) {\n"
-      "vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);\n"
-      "vec3 p = abs(fract(hsv.xxx + K.xyz) * 6.0 - K.www);\n"
-      "return hsv.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), hsv.y);\n"
-    "}\n"
-
-    "vec3 IterToRgb(int iterations) {\n"
-      "int h_deg = iterations % 360;\n"
-      "vec3 hsv = vec3(float(h_deg) / 360.0, 0.9, 1.0);\n"
-      "return HsvToRgb(hsv);\n"
-    "}\n"
-
-    "const int kMaxIterations = 100;\n"
-
-    "void main() {\n"
-      "vec2 c = v_TexCoords * 2.0 - vec2(1.5, 1.0);\n"
-      "vec2 z = c;\n"
-
-      "int i = 0;\n"
-      "for (; (i < kMaxIterations) && (ComplexSqMag(z) < 16.0); ++i) {\n"
-        "z = ComplexSq(z) + c;"
-      "}\n"
-
-      "if (i == kMaxIterations) {\n"
-        "f_Color = vec4(0.0);\n"
-      "} else {\n"
-        "f_Color = vec4(IterToRgb(i), 1.0);\n"
-      "}\n"
-    "}\n";
-// clang-format on
-
-#ifndef NDEBUG
-#  define PRINT_GL_ERROR_STATUS(info) \
-    fprintf(stdout, "Error status: %d (%s)\n", glGetError(), info)
-#else
-#  define PRINT_GL_ERROR_STATUS(...)
-#endif
+#define GM_PASS_OR_RETURN_STATUS(statement) \
+  {                                         \
+    const gm_Status kStatus = statement;    \
+    if (kStatus) {                          \
+      return kStatus;                       \
+    }                                       \
+  }                                         \
+  GM_FORCE_TRAILING_SEMICOLON_
 
 gm_Status gm_Init();
 
-gm_Status gm_CheckShaderCompilationStatus(GLuint shader);
-gm_Status gm_CheckProgramLinkStatus(GLuint program);
+typedef GLuint gm_Id;
+
+typedef struct {
+  gm_Id program;
+} gm_RenderResources;
+
+gm_Status gm_CreateProgram(gm_RenderResources *out_resources);
+
+#ifndef NDEBUG
+#  define GM_PRINT_GL_ERROR_STATUS(info) \
+    fprintf(stdout, "Error status: %d (%s)\n", glGetError(), info)
+#else
+#  define GM_PRINT_GL_ERROR_STATUS(...)
+#endif
 
 void gm_Terminate();
 
-gm_Status gm_WriteToOutput(const gm_ImageConfig *config) {
-  const gm_Status kInitStatus = gm_Init();
-  if (kInitStatus) {
-    return kInitStatus;
-  }
+gm_Status gm_Run(const gm_ImageConfig *config) {
+  GM_PASS_OR_RETURN_STATUS(gm_Init());
 
-  // Create the shaders.
-  GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-  GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-  PRINT_GL_ERROR_STATUS("create shaders");
-
-  glShaderSource(vertex_shader, 1, &kVertexShaderSource, NULL);
-  glShaderSource(fragment_shader, 1, &kFragmentShaderSource, NULL);
-  PRINT_GL_ERROR_STATUS("upload shader sources");
-
-  glCompileShader(vertex_shader);
-  glCompileShader(fragment_shader);
-  PRINT_GL_ERROR_STATUS("compile shaders");
-
-  if (gm_CheckShaderCompilationStatus(vertex_shader) ||
-      gm_CheckShaderCompilationStatus(fragment_shader)) {
-    return gm_Status_ShaderCompilationFailed;
-  }
-
-  // Create the program.
-  GLuint program = glCreateProgram();
-  PRINT_GL_ERROR_STATUS("create program");
-
-  glAttachShader(program, vertex_shader);
-  glAttachShader(program, fragment_shader);
-  PRINT_GL_ERROR_STATUS("attach shaders to program");
-
-  glLinkProgram(program);
-  PRINT_GL_ERROR_STATUS("link program");
-
-  if (gm_CheckProgramLinkStatus(program)) {
-    return gm_Status_ProgramLinkingFailed;
-  }
-
-  // Now that the program is linked, delete the shaders.
-  glDeleteShader(vertex_shader);
-  glDeleteShader(fragment_shader);
-  PRINT_GL_ERROR_STATUS("delete shaders");
+  gm_RenderResources resources;
+  GM_PASS_OR_RETURN_STATUS(gm_CreateProgram(&resources));
 
   // Create the quad data.
   // clang-format off
@@ -185,17 +99,17 @@ gm_Status gm_WriteToOutput(const gm_ImageConfig *config) {
   // Create the quad buffers.
   GLuint buffers[2];
   glGenBuffers(2, buffers);
-  PRINT_GL_ERROR_STATUS("create quad buffers");
+  GM_PRINT_GL_ERROR_STATUS("create quad buffers");
 
   // Send the data to the buffers.
   glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(kVertices), kVertices, GL_STATIC_DRAW);
-  PRINT_GL_ERROR_STATUS("upload vertex data");
+  GM_PRINT_GL_ERROR_STATUS("upload vertex data");
 
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[1]);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(kIndices), kIndices,
                GL_STATIC_DRAW);
-  PRINT_GL_ERROR_STATUS("upload index data");
+  GM_PRINT_GL_ERROR_STATUS("upload index data");
 
   // Vertex layout.
   const GLsizei kStride = 4 * sizeof(GLfloat);
@@ -206,7 +120,7 @@ gm_Status gm_WriteToOutput(const gm_ImageConfig *config) {
   const void *kTexCoordsOffset = (void *)(2 * sizeof(GLfloat));
   glEnableVertexAttribArray(1);
   glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, kStride, kTexCoordsOffset);
-  PRINT_GL_ERROR_STATUS("set vertex attribs");
+  GM_PRINT_GL_ERROR_STATUS("set vertex attribs");
 
   glBindVertexArray(0);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
@@ -216,11 +130,11 @@ gm_Status gm_WriteToOutput(const gm_ImageConfig *config) {
   GLuint image_render_buffer;
   glGenRenderbuffers(1, &image_render_buffer);
   glBindRenderbuffer(GL_RENDERBUFFER, image_render_buffer);
-  PRINT_GL_ERROR_STATUS("create image render buffer");
+  GM_PRINT_GL_ERROR_STATUS("create image render buffer");
 
   glRenderbufferStorageMultisample(GL_RENDERBUFFER, config->samples, GL_RGB8,
                                    config->size.x, config->size.y);
-  PRINT_GL_ERROR_STATUS("init image render buffer");
+  GM_PRINT_GL_ERROR_STATUS("init image render buffer");
 
   glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
@@ -228,12 +142,12 @@ gm_Status gm_WriteToOutput(const gm_ImageConfig *config) {
   GLuint render_framebuffer;
   glGenFramebuffers(1, &render_framebuffer);
   glBindFramebuffer(GL_FRAMEBUFFER, render_framebuffer);
-  PRINT_GL_ERROR_STATUS("create render framebuffer");
+  GM_PRINT_GL_ERROR_STATUS("create render framebuffer");
 
   // Attach it to the framebuffer.
   glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
                             GL_RENDERBUFFER, image_render_buffer);
-  PRINT_GL_ERROR_STATUS("attach image render buffer to render framebuffer");
+  GM_PRINT_GL_ERROR_STATUS("attach image render buffer to render framebuffer");
 
   if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
     return gm_Status_FramebufferCreationFailed;
@@ -243,26 +157,26 @@ gm_Status gm_WriteToOutput(const gm_ImageConfig *config) {
 
   // Not doing this results in the image missing some parts.
   glViewport(0, 0, config->size.x, config->size.y);
-  PRINT_GL_ERROR_STATUS("set viewport");
+  GM_PRINT_GL_ERROR_STATUS("set viewport");
 
   // Render a quad on the framebuffer.
   glBindFramebuffer(GL_FRAMEBUFFER, render_framebuffer);
-  glUseProgram(program);
+  glUseProgram(resources.program);
   glBindVertexArray(quad_va);
 
   glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, NULL);
   glBindFramebuffer(GL_FRAMEBUFFER, 0);
-  PRINT_GL_ERROR_STATUS("render the image");
+  GM_PRINT_GL_ERROR_STATUS("render the image");
 
   // Create the final image buffer.
   GLuint final_buffer;
   glGenRenderbuffers(1, &final_buffer);
   glBindRenderbuffer(GL_RENDERBUFFER, final_buffer);
-  PRINT_GL_ERROR_STATUS("create final buffer");
+  GM_PRINT_GL_ERROR_STATUS("create final buffer");
 
   glRenderbufferStorage(GL_RENDERBUFFER, GL_RGB, config->size.x,
                         config->size.y);
-  PRINT_GL_ERROR_STATUS("init final buffer");
+  GM_PRINT_GL_ERROR_STATUS("init final buffer");
 
   glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
@@ -270,18 +184,18 @@ gm_Status gm_WriteToOutput(const gm_ImageConfig *config) {
   GLuint final_framebuffer;
   glGenFramebuffers(1, &final_framebuffer);
   glBindFramebuffer(GL_FRAMEBUFFER, final_framebuffer);
-  PRINT_GL_ERROR_STATUS("create final framebuffer");
+  GM_PRINT_GL_ERROR_STATUS("create final framebuffer");
 
   glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
                             GL_RENDERBUFFER, final_buffer);
-  PRINT_GL_ERROR_STATUS("attach final buffer to final framebuffer");
+  GM_PRINT_GL_ERROR_STATUS("attach final buffer to final framebuffer");
 
   // Blit the render data to the final framebuffer.
   glBindFramebuffer(GL_READ_FRAMEBUFFER, render_framebuffer);
   glBindFramebuffer(GL_DRAW_FRAMEBUFFER, final_framebuffer);
   glBlitFramebuffer(0, 0, config->size.x, config->size.y, 0, 0, config->size.x,
                     config->size.y, GL_COLOR_BUFFER_BIT, GL_LINEAR);
-  PRINT_GL_ERROR_STATUS("blit image to final framebuffer");
+  GM_PRINT_GL_ERROR_STATUS("blit image to final framebuffer");
 
   glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 
@@ -289,12 +203,12 @@ gm_Status gm_WriteToOutput(const gm_ImageConfig *config) {
   unsigned char *data = malloc(config->size.x * config->size.y * 3);  // RGB.
 
   glBindFramebuffer(GL_READ_FRAMEBUFFER, final_framebuffer);
-  PRINT_GL_ERROR_STATUS("bind final framebuffer to read framebuffer");
+  GM_PRINT_GL_ERROR_STATUS("bind final framebuffer to read framebuffer");
 
   glReadBuffer(GL_COLOR_ATTACHMENT0);
   glReadPixels(0, 0, config->size.x, config->size.y, GL_RGB, GL_UNSIGNED_BYTE,
                data);
-  PRINT_GL_ERROR_STATUS("read image pixels");
+  GM_PRINT_GL_ERROR_STATUS("read image pixels");
 
   // Write the image data to a file.
   const int kLineStride = 3 * config->size.x;
@@ -311,9 +225,9 @@ gm_Status gm_WriteToOutput(const gm_ImageConfig *config) {
   glDeleteFramebuffers(1, &final_framebuffer);
   glDeleteRenderbuffers(1, &image_render_buffer);
   glDeleteFramebuffers(1, &render_framebuffer);
-  glDeleteProgram(program);
+  glDeleteProgram(resources.program);
   glDeleteBuffers(2, buffers);
-  PRINT_GL_ERROR_STATUS("delete objects");
+  GM_PRINT_GL_ERROR_STATUS("delete objects");
 
   gm_Terminate();
   return gm_Status_Success;
@@ -349,48 +263,31 @@ gm_Status gm_Init() {
   return gm_Status_Success;
 }
 
-const char *gm_GetStatusCodeInfo(int status_code);
+typedef struct {
+  gm_Id vertex;
+  gm_Id fragment;
+} gm_ProgramShaders;
 
-void gm_PrintStatus(FILE *stream, int status_code) {
-  const char *const kStatusInfo = gm_GetStatusCodeInfo(status_code);
-  fprintf(stream, "%s\n", kStatusInfo);
-}
+gm_Status gm_CreateShaders(gm_ProgramShaders *out_shaders);
+void gm_DestroyShaders(const gm_ProgramShaders *shaders);
+gm_Status gm_CheckProgramLinkStatus(GLuint program);
 
-const char *gm_GetStatusCodeInfo(int status_code) {
-  switch (status_code) {
-    case gm_Status_Success:
-      return "Everything went as planned";
-    case gm_Status_GlfwInitFailed:
-      return "GLFW initialization failed";
-    case gm_Status_DummyWindowCreationFailed:
-      return "Creation of the dummy window failed";
-    case gm_Status_GlLoadingFailed:
-      return "Failed to load OpenGL functions";
-    case gm_Status_ShaderCompilationFailed:
-      return "Failed to compile a shader";
-    case gm_Status_FramebufferCreationFailed:
-      return "Failed to create the image framebuffer";
-    case gm_Status_ImageOutputFailed:
-      return "Failed to write to the output image";
-    default:
-      return "Unknown status";
-  }
-}
+gm_Status gm_CreateProgram(gm_RenderResources *out_resources) {
+  gm_ProgramShaders shaders;
+  GM_PASS_OR_RETURN_STATUS(gm_CreateShaders(&shaders));
 
-gm_Status gm_CheckShaderCompilationStatus(GLuint shader) {
-  int compile_status;
-  glGetShaderiv(shader, GL_COMPILE_STATUS, &compile_status);
+  out_resources->program = glCreateProgram();
 
-  if (!compile_status) {
-    int length;
-    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+  glAttachShader(out_resources->program, shaders.vertex);
+  glAttachShader(out_resources->program, shaders.fragment);
+  GM_PRINT_GL_ERROR_STATUS("attach shaders to program");
 
-    GLchar *info_log = malloc(length);
-    glGetShaderInfoLog(shader, length, NULL, info_log);
+  glLinkProgram(out_resources->program);
+  GM_PRINT_GL_ERROR_STATUS("link program");
 
-    fprintf(stderr, "Shader compilation failed:\n%s", info_log);
-    return gm_Status_ShaderCompilationFailed;
-  }
+  // We don't need the shaders anymore.
+  gm_DestroyShaders(&shaders);
+  GM_PASS_OR_RETURN_STATUS(gm_CheckProgramLinkStatus(out_resources->program));
 
   return gm_Status_Success;
 }
@@ -413,8 +310,147 @@ gm_Status gm_CheckProgramLinkStatus(GLuint program) {
   return gm_Status_Success;
 }
 
+// clang-format off
+const char *const kGm_VertexShaderSource =
+    "#version 330 core \n"
+
+    "layout(location = 0) in vec2 a_Position;\n"
+    "layout(location = 1) in vec2 a_TexCoords;\n"
+
+    "out vec2 v_TexCoords;\n"
+
+    "void main() {\n"
+    "v_TexCoords = a_TexCoords;\n"
+    "gl_Position = vec4(a_Position, 1.0, 1.0);\n"
+    "}\n";
+
+const char *const kGm_FragmentShaderSource =
+    "#version 330 core\n"
+
+    "in vec2 v_TexCoords;\n"
+
+    "out vec4 f_Color;\n"
+
+    "vec2 ComplexMul(vec2 a, vec2 b) {\n"
+    "return vec2(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x);\n"
+    "}\n"
+
+    "vec2 ComplexSq(vec2 z) {\n"
+    "return ComplexMul(z, z);\n"
+    "}\n"
+
+    "float ComplexSqMag(vec2 z) {\n"
+    "return z.x * z.x + z.y * z.y;\n"
+    "}\n"
+
+    "vec3 HsvToRgb(vec3 hsv) {\n"
+    "vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);\n"
+    "vec3 p = abs(fract(hsv.xxx + K.xyz) * 6.0 - K.www);\n"
+    "return hsv.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), hsv.y);\n"
+    "}\n"
+
+    "vec3 IterToRgb(int iterations) {\n"
+    "int h_deg = iterations % 360;\n"
+    "vec3 hsv = vec3(float(h_deg) / 360.0, 0.9, 1.0);\n"
+    "return HsvToRgb(hsv);\n"
+    "}\n"
+
+    "const int kMaxIterations = 100;\n"
+
+    "void main() {\n"
+    "vec2 c = v_TexCoords * 2.0 - vec2(1.5, 1.0);\n"
+    "vec2 z = c;\n"
+
+    "int i = 0;\n"
+    "for (; (i < kMaxIterations) && (ComplexSqMag(z) < 16.0); ++i) {\n"
+    "z = ComplexSq(z) + c;"
+    "}\n"
+
+    "if (i == kMaxIterations) {\n"
+    "f_Color = vec4(0.0);\n"
+    "} else {\n"
+    "f_Color = vec4(IterToRgb(i), 1.0);\n"
+    "}\n"
+    "}\n";
+// clang-format on
+
+gm_Status gm_CheckShaderCompilationStatus(GLuint shader);
+
+gm_Status gm_CreateShaders(gm_ProgramShaders *out_shaders) {
+  out_shaders->vertex = glCreateShader(GL_VERTEX_SHADER);
+  out_shaders->fragment = glCreateShader(GL_FRAGMENT_SHADER);
+  GM_PRINT_GL_ERROR_STATUS("create shaders");
+
+  glShaderSource(out_shaders->vertex, 1, &kGm_VertexShaderSource, NULL);
+  glShaderSource(out_shaders->fragment, 1, &kGm_FragmentShaderSource, NULL);
+  GM_PRINT_GL_ERROR_STATUS("upload shader sources");
+
+  glCompileShader(out_shaders->vertex);
+  glCompileShader(out_shaders->fragment);
+  GM_PRINT_GL_ERROR_STATUS("compile shaders");
+
+  if (gm_CheckShaderCompilationStatus(out_shaders->vertex) ||
+      gm_CheckShaderCompilationStatus(out_shaders->fragment)) {
+    gm_DestroyShaders(out_shaders);
+    return gm_Status_ShaderCompilationFailed;
+  }
+
+  return gm_Status_Success;
+}
+
+gm_Status gm_CheckShaderCompilationStatus(GLuint shader) {
+  int compile_status;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &compile_status);
+
+  if (!compile_status) {
+    int length;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+
+    GLchar *info_log = malloc(length);
+    glGetShaderInfoLog(shader, length, NULL, info_log);
+
+    fprintf(stderr, "Shader compilation failed:\n%s", info_log);
+    return gm_Status_ShaderCompilationFailed;
+  }
+
+  return gm_Status_Success;
+}
+
+void gm_DestroyShaders(const gm_ProgramShaders *shaders) {
+  glDeleteShader(shaders->vertex);
+  glDeleteShader(shaders->fragment);
+}
+
 void gm_Terminate() {
   glfwDestroyWindow(window);
   glfwMakeContextCurrent(NULL);
   glfwTerminate();
+}
+
+const char *gm_GetStatusCodeMessage(int status_code);
+
+void gm_PrintStatus(FILE *stream, int status_code) {
+  const char *const kStatusInfo = gm_GetStatusCodeMessage(status_code);
+  fprintf(stream, "%s\n", kStatusInfo);
+}
+
+const char *gm_GetStatusCodeMessage(int status_code) {
+  switch (status_code) {
+    case gm_Status_Success:
+      return "Everything went as planned";
+    case gm_Status_GlfwInitFailed:
+      return "GLFW initialization failed";
+    case gm_Status_DummyWindowCreationFailed:
+      return "Creation of the dummy window failed";
+    case gm_Status_GlLoadingFailed:
+      return "Failed to load OpenGL functions";
+    case gm_Status_ShaderCompilationFailed:
+      return "Failed to compile a shader";
+    case gm_Status_FramebufferCreationFailed:
+      return "Failed to create the image framebuffer";
+    case gm_Status_ImageOutputFailed:
+      return "Failed to write to the output image";
+    default:
+      return "Unknown status";
+  }
 }
